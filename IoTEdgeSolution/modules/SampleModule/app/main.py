08fd1478a4f89cabbe2af03fcbdb54c23e22f1e3
Dave Glover <dglover@microsoft.com>
2020-05-19 05:16:57 +0100

    tidy

--- IoTEdgeSolution/modules/SampleModule/app/main.py
+++ IoTEdgeSolution/modules/SampleModule/app/main.py
@@ -12,22 +12,6 @@ ptvsd.enable_attach(address=('0.0.0.0', 3000))
 
 
 async def main():
-    # scopeID = None
-    # deviceId = None
-    # key = None
-
-    # scopeID = os.getenv('SCOPE_ID')
-    # deviceId = os.getenv('DEVICE_ID')
-    # key = os.getenv('DEVICE_KEY')
-
-    # if scopeID is None or deviceId is None or key is None:
-    #     sys.exit(1)
-
-    # dps = Device(scopeID, deviceId, key)
-
-    # conn_str = await dps.connection_string
-
-    # device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
     module_client = IoTHubModuleClient.create_from_edge_environment()
     await module_client.connect()
